d3c0ea2b7fcfb7f327108e7a0191fda6e2049bd4
Evan Candler <emcandler21@gmail.com>
2018-04-16 21:09:49 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,14 @@ class ApplicationController < Sinatra::Base
   get '/recipes/new' do
     erb :new
   end
+
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :show
+  end
 end
