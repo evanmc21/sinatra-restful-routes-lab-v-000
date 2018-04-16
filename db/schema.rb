7ef70c2977f70756e3cc356a3581148ad1ba8467
Evan Candler <emcandler21@gmail.com>
2018-04-16 21:29:23 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_16_204752) do
+ActiveRecord::Schema.define(version: 2018_04_16_205830) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
